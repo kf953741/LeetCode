6c241bd0b8c8cfeecb98e42ef0f4b9d888c064e2
康飞 <761551096@qq.com>
2022-05-02 21:39:28 +0800

    [123] 买卖股票的最佳时机 III

---
+++ LeetCodeAndSwordOffer/123.买卖股票的最佳时机-iii.cpp
@@ -0,0 +1,48 @@
+/*
+ * @lc app=leetcode.cn id=123 lang=cpp
+ *
+ * [123] 买卖股票的最佳时机 III
+ */
+
+// @lc code=start
+class Solution
+{
+public:
+    int maxProfit(vector<int> &prices)
+    {
+        int n = prices.size();
+        if (n == 0 || n == 1)
+        {
+            return 0;
+        }
+        int k = 3;
+        vector<vector<int>> hold(n, vector<int>(k));
+        vector<vector<int>> unhold(n, vector<int>(k));
+        
+        for (size_t i = 0; i < n; i++)
+        {
+            hold[i][0] = 0;
+            unhold[i][0] = 0;
+        }
+         for (size_t i = 1; i < k; i++)
+        {
+            hold[0][i] = -prices[0];
+            unhold[0][i] = 0;
+        }
+        
+       
+        int ret = INT_MIN;
+        for (size_t i = 1; i < n; i++)
+        {
+            for (size_t j = 1; j < k; j++)
+            {
+                hold[i][j] = max(hold[i - 1][j], unhold[i - 1][j - 1] - prices[i]);
+                unhold[i][j] = max(unhold[i - 1][j], hold[i - 1][j] + prices[i]);
+                int curMax = max(hold[i][j], unhold[i][j]);
+                ret = max(ret, curMax);
+            }
+        }
+        return ret;
+    }
+};
+// @lc code=end
